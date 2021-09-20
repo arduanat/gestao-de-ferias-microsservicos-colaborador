1be46fc9a04950beeb9f277e4b6e1e8245774c62
Jonatas Legal <jonataslegal@outlook.com>
2021-09-19 22:15:38 -0400

    api para microsserviço de colaboradores

---
+++ src/Dominio/Context/Contexto.cs
@@ -0,0 +1,28 @@
+﻿using Dominio.Context.Mapping;
+using Dominio.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Dominio.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        public Contexto()
+        {
+        }
+
+        public DbSet<Colaborador> Colaborador { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+
+            modelBuilder.ApplyConfiguration(new ColaboradorMap());
+        }
+    }
+}
---
+++ src/Dominio/Models/Colaborador.cs
@@ -0,0 +1,20 @@
+﻿namespace Dominio.Models
+{
+    public class Colaborador
+    {
+        public Colaborador(string nome, string matricula, int id = 0)
+        {
+            Id = id;
+            Nome = nome;
+            Matricula = matricula;
+        }
+
+        public Colaborador()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+    }
+}
